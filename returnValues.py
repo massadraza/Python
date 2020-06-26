a094061a1eb0f71916dc740e5cc079c1a17b5b18
Massad  Raza <massadraza@macbook-pro.myfiosgateway.com>
2020-06-26 16:30:56 -0400

    first commit

---
+++ returnValues.py
@@ -0,0 +1,9 @@
+def game_allow(my_age):
+    user_allow = my_age
+    return user_allow
+
+massads_limit = game_allow(13)
+print ('Massad can not play this game because he is', massads_limit) 
+print ('Massad will need to wait to be 18 years or older in order to play this game!')
+
+
