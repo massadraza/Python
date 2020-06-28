9baef7369596598a46a0475d3cc3f6d8a2bc32f3
Massad  Raza <massadraza@macbook-pro.myfiosgateway.com>
2020-06-28 18:50:35 -0400

    first commit

---
+++ keyword_Arguements.py
@@ -0,0 +1,7 @@
+def What_Did_Massad_Do(name="Massad", action="ate", food="apples"):
+    print (name, action, food)
+
+What_Did_Massad_Do()
+What_Did_Massad_Do('Bob', 'played', 'soccer') 
+What_Did_Massad_Do (action= 'ran two blocks down the road', food='to eat Dukin Dounts')
+What_Did_Massad_Do (food='to eat Dukin Dounts', action= 'ran two blocks down the road') 
---
+++ variableScope.py
@@ -0,0 +1,11 @@
+a = 24895
+
+def wow(): 
+    print (a)
+
+def cool(): 
+    print (a)
+
+wow()
+cool()
+
