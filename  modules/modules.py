6079f1ba3f190f1dda548f6b9f93f0a39b5bc25a
Massad  Raza <massadraza@MacBook-Pro.local>
2020-06-30 17:57:18 -0400

    first commit

---
+++  modules/modules.py
@@ -0,0 +1,4 @@
+def Mac():
+    print ('Welcome To MacBook, the reinvented laptop since the 21st Century!')
+
+Mac() 
---
+++ dictionary.py
@@ -0,0 +1,7 @@
+students = {'Matt': 'a cool kid', 'Josh': 'an average who doesn\'t make sense', 'George': 'sits in front of me in math class'}
+
+print (students) 
+print (students['Matt'])
+
+for k, v in students.items(): 
+    print ('The defintion of', k, 'is', v) 
