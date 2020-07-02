c20bd3ca39cd24bd6e320ee9df466388c6e7cc8d
Massad  Raza <massadraza@massads-mbp.myfiosgateway.com>
2020-07-02 18:00:06 -0400

    first commit

---  modules/secondModules.py
+++  modules/secondModules.py
@@ -1,3 +1,8 @@
 import modules
-
 modules.Mac() 
+
+import random 
+x = random.randrange(1,500)
+print (x) 
+
+
---
+++ DownloadImagesProject/downloading_Images.py
@@ -0,0 +1,9 @@
+import random 
+import urllib.request 
+
+def download_image(url):
+    name = random.randrange(1,500)
+    full_name = str(name) + '.jpg'
+    urllib.request.urlretrieve(url, full_name) 
+
+download_image('https://www.python.org/static/community_logos/python-logo-master-v3-TM.png') 
