47169ae5006a78f4a9c6e6b966519de4e5430a88
Massad  Raza <massadraza@massads-mbp.myfiosgateway.com>
2020-07-03 17:58:57 -0400

    first commit

---
+++ CreatingFiles/howtoreadandwritefiles.py
@@ -0,0 +1,8 @@
+fw = open("example.txt", 'w') 
+fw.write("I am testing to see if you can edit files using Python\n")
+fw.write('Testing, testing, 1,2,3')
+fw.close() 
+
+fr = open('example.txt', 'r')
+text = fr.read()
+print (text) 
---
+++ Downloading_Files/downloading_files.py
@@ -0,0 +1,16 @@
+from urllib import request 
+
+APPL_url = 'https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=1562188252&period2=1593810652&interval=1d&events=history'
+
+def download_APPL_stock_info(csv_url):
+    response = request.urlopen(csv_url)  
+    csv = response.read() 
+    csv = str(csv)
+    lines = csv.split("\\n") 
+    dest_url = r'APPL.csv' 
+    fx = open(dest_url, 'w') 
+    for line in lines:
+        fx.write(line + "\n") 
+    fx.close() 
+
+download_APPL_stock_info(APPL_url) 
