ac6c77daf0a0032e55607813c16f6930a565dbe0
Massad  Raza <massadraza@massads-mbp.myfiosgateway.com>
2020-07-09 17:55:44 -0400

    Learning how to read and write files using Python Code

--- CreatingFiles/howtoreadandwritefiles.py
+++ CreatingFiles/howtoreadandwritefiles.py
@@ -6,4 +6,4 @@ fw.close()
 fr = open('example.txt', 'r')
 text = fr.read()
 print (text) 
-#  Learning how to read and write files using Python Code.
+#   Learning how to read and write files using Python Code.
