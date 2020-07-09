b14ec285a2a37851c26645354efe09b46e74b09e
Massad  Raza <massadraza@massads-mbp.myfiosgateway.com>
2020-07-09 17:39:57 -0400

    Learning how to use the keyword return

--- returnValues.py
+++ returnValues.py
@@ -6,5 +6,5 @@ massads_limit = game_allow(13)
 print ('Massad can not play this game because he is', massads_limit) 
 print ('Massad will need to wait to be 18 years or older in order to play this game!')
 
-#Learning how to use the keyword return.
+# Learning how to use the keyword return.
 
