643581132a3a514efcb5ac0298fe7e43437ec213
Massad  Raza <massadraza@massads-mbp.myfiosgateway.com>
2020-07-10 17:34:43 -0400

    Building a WebCrawler using Python code

--- BuildingAWebCrawler/webCrawler.py
+++ BuildingAWebCrawler/webCrawler.py
@@ -12,18 +12,7 @@ def item_spider(max_pages):
         for link in soup.findAll('a', {'class': 's-item__link'}):  
             print (link.h3.get_text()) 
             print (link["href"]) 
-            #get_item_info(soup)           
         page += 1
-
-
-def get_item_info(item_url):
-    source_code = requests.get(item_url)  
-    plain_text = source_code.text 
-    soup = BeautifulSoup(plain_text,features="html.parser")
-    for item_name in soup.findAll('h1', {'class': 'it-ttl'}):
-        print (item_name.string)   
-    for link in soup.findAll('h1'):
-        print (link["href"])
 
 item_spider(1)  
 
