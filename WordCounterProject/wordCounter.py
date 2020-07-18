282bc4029ce7dbc23d03c0304c138a36eb6c2c9a
Massad  Raza <massadraza@massads-mbp.myfiosgateway.com>
2020-07-18 15:02:17 -0400

    Building a Word Frequency Counter which is able to take links from a web page and count how many words are on the page and organize them

--- WordCounterProject/wordCounter.py
+++ WordCounterProject/wordCounter.py
@@ -1,29 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
-import operator 
+import operator
 
 def start(url):
     word_list = []
-    source_code = requests.get(url).text 
+    source_code = requests.get(url).text
     soup = BeautifulSoup(source_code, features="html.parser") 
-    for post_text in soup.findAll('a', {'class': "style-scope ytd-comment-renderer"}):
-        content = post_text.string 
-        words = content.lower().split()
-        for each_word in words:
-            word_list.append(each_word)
-    clean_up_list(word_list) 
+    for website_text in soup.findAll('a', {'class': 'js-card__link link-gray'}):  
+        if website_text.string is not None:
+            content = website_text.string
+            words = content.lower().split()
+
+            for each_word in words:
+                word_list.append(each_word)
+
+    clean_up_list(word_list)
 
 def clean_up_list(word_list):
     clean_word_list = []
     for word in word_list:
-        symbols = "!@#$%^&*()_+{}:\"<>?,./;'[]-='" 
-        for x in range(0, len(symbols)): 
-            word = word.replace(symbols[x], "") 
+        symbols = "!@#$%^&*()_+-={}:\"<>?[];',./'`~"
+        for i in range(len(symbols)):
+            word = word.replace(symbols[i], "")
         if len(word) > 0:
-            print(word)
             clean_word_list.append(word)
 
+    create_dictionary(clean_word_list)
 
-start('https://www.youtube.com/watch?v=ZxiJ92-4Qys&list=PL6gx4Cwl9DGAcbMi1sH6oAMk4JHw91mC_&index=36&t=70s')
+def create_dictionary(clean_word_list):
+    word_count = {}
+    for word in clean_word_list:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
 
-# Building a Word Frequency Counter using Python Code
+    for k,v in sorted(word_count.items(), key=operator.itemgetter(1)):
+        print(k,":",v)               
+
+
+start('https://www.buzzfeed.com/')  
+
+# Building a Word Frequency Counter which is able to take links from a web page and count how many words are on the page and organize them. 
