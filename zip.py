e0225819654339e23b815a005aaf255f16e7f0ca
Massad  Raza <massadraza@massads-mbp.myfiosgateway.com>
2020-07-21 15:06:55 -0400

    Learing how to zip two lists together

---
+++ zip.py
@@ -0,0 +1,10 @@
+names = ["Bob", "Massad", "Richard", "Michael"]
+idnumber = [53245, 53242, 32244, 42244]
+
+Personal_Info = zip(names, idnumber)
+
+for a, b in Personal_Info:
+    print(a, b)  
+
+# Learing how to zip two lists together
+
