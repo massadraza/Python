ebb9231a631f671639c0354d9a0e8b20ee20f648
Massad  Raza <massadraza@massads-mbp.myfiosgateway.com>
2020-07-22 18:13:38 -0400

    Learning how to print out image details and show an image using Python Code

---
+++ Editing_Images/pillow.py
@@ -0,0 +1,8 @@
+from PIL import Image
+
+img = Image.open("/Users/massadraza/Desktop/Python/Editing_Images/macOS.png") 
+print(img.size)
+print(img.format)  
+img.show()
+
+# Learning how to print out image details and show an image using Python Code
