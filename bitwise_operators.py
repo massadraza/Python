d43d24a74744b26301e991437fc9297e2ae0e6af
Massad  Raza <massadraza@massads-mbp.myfiosgateway.com>
2020-07-28 16:50:49 -0400

    Learning Binary And and Binary Right Shift

---
+++ bitwise_operators.py
@@ -0,0 +1,17 @@
+
+# --------------- Binary And --------------
+
+a = 50              # 110010
+b = 25              # 011001
+c = a & b           # 010000
+
+print(c) 
+
+# --------------- Binary RIGHT SHIFT --------------
+
+x = 250        # 11111010
+y = x >> 2     # 00111110
+
+print(y) 
+
+# Learning Binary And and Binary Right Shift. 
