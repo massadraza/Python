9035d2aef802cd609aa9d5388c64c93d49a8f3fa
Massad  Raza <massadraza@massads-mbp.myfiosgateway.com>
2020-07-28 16:51:28 -0400

    Learning about the map function.

---
+++ map.py
@@ -0,0 +1,9 @@
+Students_in_class = [28, 26, 30, 45]
+
+def new_students(students):
+    return students * 2
+
+new_students_list = list(map(new_students, Students_in_class))
+print (new_students_list) 
+
+# Learning about the map function. 
